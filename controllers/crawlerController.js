0d0d2666a52a3eaffee0c7a7e413047de7327d1f
alex alonso <alex.xmde@gmail.com>
2017-03-08 08:45:09 -0300

    Code organized, not tested because the target website is offline

--- controllers/crawlerController.js
+++ controllers/crawlerController.js
@@ -1,26 +1,127 @@
 let cheerio = require('cheerio')
 let request = require('request')
 let dbController = require('./dbController')
+let categoryModel = require('../models/category')
+let productModel = require('../models/product')
+let mongoose = require('mongoose')
 
 
 function CrawlerController() {
-  this.process = () => {
-    return new Promise((resolve, reject) => {
-      request('http://www.artmoveistubular.com.br/', (error, response, body) => {
-        let $ = cheerio.load(body)
-        let categoriesNode = $('ol').find('li h2 span')
-        console.log(categoriesNode.length)
-          categoriesNode.each((i, elem) => {
-            let category = $(elem).text()
-            console.log(category)
+  this._baseUrl = 'http://www.artmoveistubular.com.br' 
+  this._dbConnect = () => {
+    mongoose.connect('mongodb://localhost/loja')
+  }
 
+  this._getProducts = (url) => {
+    return new Promise((resolve, reject) => {
+      let tryParse = () => {
+        request(url, (error, response, body) => {
+          if(response.statusCode == '503') {
+            return tryParse()
+          }
+
+          let $ = cheerio.load(body)
+          let productsNode = $('#tum .post')
+          let operations = []
+          productsNode.each((i, elem) => {
+            let productUrl = $(elem).find(a).attr('href')
+            operations.push(this._getSingleProduct(productUrl))
           })
 
-
-
-      })
+          let fetchProducts = Promise.all(operations)
+          fetchProducts.then(products => {
+            resolve(products)
+          })
+        })
+      }
     })
   }
+
+  this._getSingleProduct = (url) => {
+    return new Promise((resolve, reject) => {
+      let tryParse = () => {
+        request(url, (error, response, body) => {
+          if (response.statusCode == '503'){ 
+            return tryParse()
+          }
+
+          let $ = cheerio.load(body)
+          let product = new productModel({
+            name: $('#nome-do-produto-detalhe').text().trim(),
+            description: $('#texto-especifica').text().trim()
+          })
+        //TODO download images
+          resolve(product)
+
+        })
+      }
+    })
+  }
+  this._getCategories = (url) => {
+    return new Promise((resolve, reject) => {
+      let tryParse = () =>  {
+        request(this._baseUrl, (error, response, body) => {
+          if(response.statusCode == '503') {
+            return tryParse()
+          }
+          let $ = cheerio.load(body)
+          let categoriesNode = $('ol li')
+          let categories = []
+          categoriesNode.each((i, elem) => {
+
+            let category = new categoryModel({
+              name: $(elem).find('h2').text(),
+              url: this._baseUrl + $(elem).find('div figure a').attr('href')
+            })
+
+            categories.push(category)
+
+          })
+          resolve(categories)
+        })
+      }
+      tryParse()
+    })
+  }
+
+
+  this.process = () => {
+    return new Promise((resolve, reject) => {
+      this._getCategories(this._baseUrl).then(categories => {
+        categories.forEach((c) => {
+          c.save().then(() => {
+            this._getProducts(c.url).then(products => {
+              products.forEach(product => {
+                product.save().then(product => {
+                  console.log('inserted product ' + product.name)
+                })
+              })
+            })
+          })
+        })
+      })
+    })
+    console.log("Loggin")
+    request(linkToProducts, (error, response, body) => {
+      if(response.statusCode === '503') {
+        return tryParse()
+      }
+      $ = cheerio.load(body, {decodeEntities: true})
+
+      this._execProducts(productsNode, category )
+
+    })//request
+  }//tryparse
+
+  tryParse()
+}) //category.save
+  }) //categoriesNode.each
+})  //request
+
+
+
+  })
+}
 }
 
 module.exports = new CrawlerController()
--- models/category.js
+++ models/category.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 let schema = new mongoose.Schema({
   name: {type: String, required: true},
   description: {type:String, required: true},
-  products: [{type: mongoose.Schema.Types.ObjectId, ref:'Product'}]
+  products: [{type: mongoose.Schema.Types.ObjectId, ref:'Product'}],
+  url: String //OOOOOOOOOOOOOOOOO GAMBIARA
 });
 
 module.exports = mongoose.model('Category',schema);
