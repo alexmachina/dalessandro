ae8f09eb8e02408a8657490ef14b29ffa1774245
alex alonso <alex.xmde@gmail.com>
2017-06-25 17:45:12 -0300

    Acho que agora vai

--- controllers/crawlerController.js
+++ controllers/crawlerController.js
@@ -4,6 +4,9 @@ let dbController = require('./dbController')
 let categoryModel = require('../models/category')
 let productModel = require('../models/product')
 let mongoose = require('mongoose')
+let short = require('shortid')
+let fs = require('fs')
+let http = require('http')
 
 
 function CrawlerController() {
@@ -12,20 +15,25 @@ function CrawlerController() {
     mongoose.connect('mongodb://localhost/loja')
   }
 
-  this._getProducts = (url) => {
+  this._getProducts = (url, categoryId) => {
     return new Promise((resolve, reject) => {
       let tryParse = () => {
+        console.log('------- parsing category ------: ' + url)
         request(url, (error, response, body) => {
           if(response.statusCode == '503') {
             return tryParse()
+            
           }
+
 
           let $ = cheerio.load(body)
           let productsNode = $('#tum .post')
           let operations = []
+
           productsNode.each((i, elem) => {
-            let productUrl = $(elem).find(a).attr('href')
-            operations.push(this._getSingleProduct(productUrl))
+            let imageUrl = $(elem).find("#img-bloco a img").attr('src')
+            let productUrl = $(elem).find('#bloco a').attr('href')
+            operations.push(this._getSingleProduct(productUrl, imageUrl, categoryId))
           })
 
           let fetchProducts = Promise.all(operations)
@@ -34,32 +42,73 @@ function CrawlerController() {
           })
         })
       }
+        tryParse()
+      })
+  }
+
+  this._downloadImage = (url) => {
+    return new Promise((resolve, reject) => {
+      let path = 'img/products/'+short.generate()
+      let file = fs.createWriteStream(path)
+      let tryParse = () => {
+        console.log('parsing image: ' + url)
+      http.get(url, response => {
+        if(response.statusCode  == '503') {
+          return tryParse()
+        }
+        response.pipe(file)
+        debugger
+        resolve(path.split('/')[2])
+      }).on('error', (err) => {
+        reject(err)})
+      }
+      tryParse()
     })
   }
 
-  this._getSingleProduct = (url) => {
+  this._getSingleProduct = (url, imageUrl, categoryId) => {
     return new Promise((resolve, reject) => {
       let tryParse = () => {
+        console.log('parsing product: ' + url)
         request(url, (error, response, body) => {
           if (response.statusCode == '503'){ 
             return tryParse()
           }
 
           let $ = cheerio.load(body)
-          let product = new productModel({
-            name: $('#nome-do-produto-detalhe').text().trim(),
-            description: $('#texto-especifica').text().trim()
+
+          this._downloadImage(imageUrl).then(filename => {
+            let product = new productModel({
+              name: $('#nome-do-produto-detalhe').text().trim(),
+              mainImage: filename,
+              description: $('#texto-especifica').text().trim(),
+              category: categoryId,
+              active: true,
+              featured: false
+            })
+
+            product.save((err, prod) => {
+              if(!err) {
+
+                console.log(prod.name + ' Saved')
+              }
+            })
+
+
           })
-        //TODO download images
-          resolve(product)
+
+
 
         })
       }
+      tryParse()
     })
   }
   this._getCategories = (url) => {
+
     return new Promise((resolve, reject) => {
       let tryParse = () =>  {
+        console.log('parsing categories: ' + url)
         request(this._baseUrl, (error, response, body) => {
           if(response.statusCode == '503') {
             return tryParse()
@@ -87,15 +136,13 @@ function CrawlerController() {
 
   this.process = () => {
     return new Promise((resolve, reject) => {
+      mongoose.Promise = Promise;
+      mongoose.connect('mongodb://localhost/loja')
       this._getCategories(this._baseUrl).then(categories => {
         categories.forEach((c) => {
-          c.save().then(() => {
-            this._getProducts(c.url).then(products => {
-              products.forEach(product => {
-                product.save().then(product => {
-                  console.log('inserted product ' + product.name)
-                })
-              })
+          c.save((err, c)=> {
+            this._getProducts(c.url, c._id).then(products => {
+              console.log("End")
             })
           })
         })
@@ -113,15 +160,6 @@ function CrawlerController() {
     })//request
   }//tryparse
 
-  tryParse()
-}) //category.save
-  }) //categoriesNode.each
-})  //request
-
-
-
-  })
-}
 }
 
 module.exports = new CrawlerController()
--- models/category.js
+++ models/category.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 let schema = new mongoose.Schema({
   name: {type: String, required: true},
-  description: {type:String, required: true},
+  description: {type:String },
   products: [{type: mongoose.Schema.Types.ObjectId, ref:'Product'}],
   url: String //OOOOOOOOOOOOOOOOO GAMBIARA
 });
